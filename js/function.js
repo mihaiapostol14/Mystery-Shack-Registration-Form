2df11c668785476b00584b0cc62bfe1e84a0bb89
Mihai Apostol <mihaiapostol153@gmail.com>
2024-10-31 11:10:32 +0200

    add js dir

---
+++ js/function.js
@@ -0,0 +1,13 @@
+let InputArray = []
+
+let emailInput = document.getElementById("emailInput");
+let passwordInput = document.getElementById("passwordInput");
+let  Button = document.getElementById("Buttonsubmit");
+InputArray.push(
+    emailInput.value,
+    passwordInput.value,
+)
+Button.addEventListener("click",onClickButton())
+ function name(params) {
+     
+ }
